9c24345581a5c741d589d69aca04d036add0d711
Roan Mertens <roanmertens91@gmail.com>
2018-06-26 15:08:37 +0100

    made answers more varied

--- 1widgets.rb
+++ 1widgets.rb
@@ -1,13 +1,13 @@
 def price(amount)
 	if amount < 0
 		"That is not enought!"
-	elsif amount < 10
+	elsif amount > 0 && amount < 10
 		amount * 10
-	elsif amount < 50
+	elsif amount > 0 && amount < 50
 		amount * 9
-	elsif amount < 100
+	elsif amount > 0 && amount < 100
 		amount * 8
-	elsif amount >= 100
+	elsif amount > 0 && amount >= 100
 		amount * 7
 	else
 		"That is not an Integer!"
@@ -19,9 +19,19 @@ def answer(ans)
 	gets.chomp.to_i
 end
 
+def back(ans1, ans2)
+	if ans1 == 0
+		ans2
+	else
+		"The price for #{ans1} is #{ans2} coins"
+	end
+end
+
 puts "This is the widget store!"
 puts "A widget costs 10 coins."
 puts "But we might be able to make a deal if you buy enough widgets."
 a = answer("How many widgets do you want?")
 
-puts "The price for #{a} is #{price(a)} coins"
+c = price(a)
+
+puts back(a, c)
