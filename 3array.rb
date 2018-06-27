b1df898657c3e767571f0417ff3df34dc09dc1c7
Roan Mertens <roanmertens91@gmail.com>
2018-06-27 08:35:36 +0100

    added an array and improved upond the myself method. also iterated over friends.

--- 3array.rb
+++ 3array.rb
@@ -1,3 +1,9 @@
+#friends array.
+friends = ["koen", "michael", "Tien", "Vincent", "Tom", "Wouter", "Hans"]
+
+
+
+#made a hash by mistake. Still going to use it in the exercize!
 family = {
 	mother: "jaq",
 	father: "ton",
@@ -5,12 +11,22 @@ family = {
 	i: "roan",
 }
 
+#method to recognize myself from my family.
 def myself(k, v)
 	if v == "roan"
-		puts "And #{k} am #{v}."
+		puts "And #{k.capitalize} am #{v.capitalize}."
 	else
-		puts "my #{k} is #{v}."
+		puts "my #{k} is #{v.capitalize}."
 	end
 end
 
+a = 0
 family.each{|key,val| myself(key, val)}
+friends.each do |name| a += 1
+  if a == friends.length
+	puts "And last but not least."
+	puts "#{name.capitalize} is a friend of me."
+  else
+	puts "#{name.capitalize} is a friend of me."
+  end
+end
