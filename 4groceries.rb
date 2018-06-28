8720e3abae124e06bb8c420694b414d0d5a84c68
Roan Mertens <roanmertens91@gmail.com>
2018-06-28 09:28:32 +0100

    old commit groceries

--- 4groceries.rb
+++ 4groceries.rb
@@ -1,23 +1,38 @@
 groceries = Hash.new
-food_list = Hash.new
-food = 0
 
-puts "When do you want to do groceries?"
-day = gets.chomp.to_sym
-ans = "n"
-
-until ans == "y" do
-  	puts "What do you want to buy?"
-  	food = gets.chomp
-  	puts "how much #{food} do you want?"
-  	amount = gets.chomp.to_i 
-  	food_list[food] = amount
-  	puts "do you want to stop? y or n"
-  	ans = gets.chomp
+def day
+  puts "When do you want to do groceries?"
+  day = gets.chomp.to_sym
 end
 
-groceries[day] = food_list
+def add
+  food_list = Hash.new
+  food = 0
+  ans = "n"
+  until ans == "y" do
+    puts "What do you want to buy?"
+    food = gets.chomp
+    puts "how much #{food} do you want?"
+    amount = gets.chomp.to_i 
+  	food_list[food] = amount
+  	puts "do you want to stop? y or n"
+    ans = gets.chomp
+  	food_list
+  end
+  return food_list
+end
 
+def add_to_groceries
+	grocery = Hash.new
+	ans1 = day
+	ans2 = add
+	grocery[ans1] = ans2
+	return grocery
+end
+
+groceries.merge!(add_to_groceries)
 
 puts groceries
 
+groceries.each{|key, val| puts "The grocery list for #{key} consists of:"}
+groceries.each{|key, val| puts val}
