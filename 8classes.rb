caf016b6d1aee7f52f59b7ef5eda2dabc1834a86
Roan Mertens <roanmertens91@gmail.com>
2018-07-08 09:36:54 +0100

    changed the name into first middle an last name and fullname in the Human class

--- 8classes.rb
+++ 8classes.rb
@@ -1,42 +1,59 @@
 
-class Human
-
-	def initialize(name, age)
-		@name = name
-		@age = age
-	end
-	def givename
-		puts @name
-	end
-	def giveage
-		puts @age
-	end
-	def giveboth
-		puts "#{@name} is #{@age} years old."
-	end
-end
-
-newhuman = Human.new("Bert", 12)
-
-puts newhuman.givename
-puts newhuman.giveage
-puts newhuman.giveboth
-
-
+#basic who class with no possible changes between the instances of the class.
 class Who
 
 	def firstname
-		"Bert"
+		"Jan"
 	end
 	def middlename
 		""
 	end
 	def lastname
-		"bruhman"
+		"Alleman"
 	end
 end
-
 
 berty = Who.new
 
 puts "#{berty.firstname} #{berty.middlename} #{berty.lastname}"
+puts ""
+
+
+#Human class with instances that are possible to make unique.
+class Human
+
+	def initialize(firstname, middlename, lastname, age)
+		@firstname = firstname
+		@middlename = middlename
+		@lastname = lastname
+		@age = age
+	end
+	def givefirstname
+		puts @firstname
+	end
+	def givemiddlename
+		puts @middlename
+	end
+	def givelastname
+		puts @lastname
+	end
+	def givefullname
+		puts "#{@firstname} #{@middlename} #{@lastname}"
+	end
+	def giveage
+		puts @age
+	end
+	def giveboth
+		puts "#{@firstname} is #{@age} years old."
+	end
+end
+
+newhuman = Human.new("Bert", "Bernard", "Brum", 12)
+
+newhuman.givefirstname
+newhuman.givemiddlename
+newhuman.givelastname
+newhuman.givefullname
+newhuman.giveage
+newhuman.giveboth
+
